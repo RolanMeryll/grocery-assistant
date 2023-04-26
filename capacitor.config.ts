8a09a153c2c0ffb69fd4b0f4ceb30ede1024cde1
RolanMeryll <donnille012@gmail.com>
2023-04-26 22:00:51 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'grocery-assistant',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
